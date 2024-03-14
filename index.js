2c76d7495c56979d63b3e3b135bf8cebc4434c42
Albert Shih <albertshih3@gmail.com>
2024-03-14 16:30:00 -0700

    Remove unnecessary code
    Removed CatTracks paths from index as they are not needed anymore.

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const status = require('./status.js');
 const incidents = require('./incidents.js');
-const cattracks = require('./cattracks.js');
-const cattracks_route = require('./cattracks_route.js');
-const routeplanner = require('./routeplanner.js');
 
 // instantiate the express server
 const app = express();
@@ -40,37 +37,6 @@ app.get('/status/incidents/:id', async function (req, res) {
     let currentStatus = await incidents.getStatusData();
     console.log(req.params);
     res.json(incidents.makeIncidentDetails(req.params.id));
-})
-
-app.get('/cattracks', async function (req, res) {
-    await cattracks.getRouteData();
-    await cattracks.getSchedule();
-    await cattracks.getStops();
-    console.log(req.headers);
-    console.log(req.query);
-    res.json(cattracks.buildHome(req.query));
-    console.log("ALL DONE! 🎉")
-})
-
-app.post('/cattracks/routeplanner', async function (req, res) {
-    console.log(req.headers);
-    console.log(req.query);
-    console.log(req.body);
-
-    await routeplanner.getRouteData();
-    await routeplanner.getSchedule();
-    await routeplanner.getStops();
-
-    res.json(routeplanner.buildRoutePlan(req.body));
-})
-
-app.get('/cattracks/route/:id', async function (req, res) {
-    await cattracks_route.getRouteData();
-    await cattracks_route.getSchedule();
-    console.log(req.headers);
-    let xmJson = cattracks_route.buildRouteInformation(req.params.id);
-    res.json(xmJson);
-    console.log("ALL DONE! 🎉")
 })
 
 module.exports.handler = serverless(app);
